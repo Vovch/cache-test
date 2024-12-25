7e7bc5980ab002ba18e19bfd2d34bea31bf6a2d7
Vladimir Mironov <vladimir_mironov2@rntgroup.com>
2024-12-25 20:02:18 +0300

    set counter as client component

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -1,14 +1,27 @@
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
+'use client'
+
+import { useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation'
 
 const fetchRandomNumbers = async () => {
-    const response = await fetch("http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=1", { cache: 'no-store', next: { revalidate: 0 } });
-    const data = await response.json();
-    return data;
+    const response = await fetch("https://www.random.org/integers/?num=1&min=100&max=1000&col=1&base=10&format=plain&rnd=new", {
+        cache: 'no-store'
+    });
+    const data = await response.text();
+    return [parseInt(data.trim())];
 };
 
-export default async function Counter() {
-    const numbers = await fetchRandomNumbers();
+export default function Counter() {
+    const [numbers, setNumbers] = useState<number[]>([]);
+    const pathname = usePathname();
+
+    useEffect(() => {
+        const getNewNumber = async () => {
+            const newNumbers = await fetchRandomNumbers();
+            setNumbers(newNumbers);
+        };
+        getNewNumber();
+    }, [pathname]); // Re-run effect when pathname changes
 
     return (
         <div>
